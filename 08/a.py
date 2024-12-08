bd78cf03f1d8cb73b916b78b3f5d51f046b70376
Jack Deadman <jackdeadman1@gmail.com>
2024-12-08 23:00:24 +0000

    Use product for a.py

--- 08/a.py
+++ 08/a.py
@@ -1,6 +1,6 @@
 from typing import Iterable
 
-from itertools import combinations
+from itertools import product
 
 from pathlib import Path
 
@@ -11,13 +11,9 @@ def find_antinode(s1: Satellite, s2: Satellite) -> complex:
     return s1.position + delta
 
 def find_all_antinodes(satellites: list[Satellite], grid_shape: GridShape) -> Iterable[complex]:
-    for s1, s2 in combinations(satellites, 2):
-
-        if (anti1 := find_antinode(s1, s2)) in grid_shape:
+    for s1, s2 in product(satellites, repeat=2):
+        if s1 != s2 and (anti1 := find_antinode(s1, s2)) in grid_shape:
             yield anti1
-
-        if (anti2 := find_antinode(s2, s1)) in grid_shape:
-            yield anti2
 
 def solve(satellites: list[Satellite], grid_shape: GridShape) -> int:
 
